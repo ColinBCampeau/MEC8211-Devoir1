83ab5852171f682bdf6d69076f4e8d2e157b87ae
ermohammad <eroj.mohammad-ishoq@polymtl.ca>
2024-03-25 00:25:59 -0400

    update de question b

--- Devoir3/src/QUESTION_B.m
+++ Devoir3/src/QUESTION_B.m
@@ -1,37 +1,42 @@
-%% MONTE CARLO AVEC ECHANTILLONNAGE HYPERCUBE LATIN (LHS)
+clc
+clear all
+close all
 
-% Définir le nombre d'échantillons pour LHS
-num_samples = 100;
+%% MONTE CARLO
 
-% Définir la moyenne et l'écart type pour la porosité pour LHS
+% Définir le nombre d'échantillons
+num_samples = 300;
+
+% Définir la moyenne et l'écart type pour la porosité
 mean_porosity = 0.900; % Moyenne de la porosité
 std_porosity = 7.50e-3; % Écart type de la porosité
 
-% Réaliser l'échantillonnage hypercube latin pour la porosité
-porosity_samples = lhsnorm(mean_porosity, std_porosity^2, num_samples);
+% Réaliser l'échantillonnage pour la porosité
+porosity_samples = normrnd(mean_porosity, std_porosity, [1,num_samples]);
 
 % Préallouer le tableau pour les résultats de perméabilité
 permeability_results = zeros(num_samples, 1);
 
 % Définir les paramètres fixes pour la simulation
-seed = 101; % Remplacer par la valeur de seed appropriée
+seed = 0; % Valeur du seed
 mean_diameter = 12.5; % Diamètre moyen des fibres en microns
 std_diameter = 2.85; % Écart type du diamètre des fibres en microns
-NX = 100; % Remplacer par la taille latérale appropriée du domaine en cellules de grille
+NX = 100;
 dx = 2e-6; % Taille de la grille en mètres
-deltaP = 0.1; % Chute de pression en Pascal
-filename = 'fiber_mat.tiff'; % Nom du fichier pour l'image de la structure des fibres
+deltaP = 0.1; % delta pression en Pascal
+filename = 'fiber_mat.tiff'; % Filename for fiber structure image
 
 % Boucler sur les échantillons LHS et exécuter les simulations
 for i = 1:num_samples
-    % Extraire l'échantillon de porosité actuel
-    current_porosity = porosity_samples(i);
     
-    % Générer la structure des fibres pour l'échantillon de porosité actuel
-    [d_equivalent] = Generate_sample(seed, filename, mean_diameter, std_diameter, current_porosity, NX, dx);
+    % Générer la structure des fibres pour l'échantillon de porosité 
+    [d_equivalent] = Generate_sample(seed, filename, mean_diameter, std_diameter, porosity_samples(i), NX, dx);
     
     % Exécuter la simulation LBM pour obtenir la perméabilité
     permeability_results(i) = LBM(filename, NX, deltaP, dx, d_equivalent);
+
+    disp("i = ")
+    disp(i)
 end
 
 %% GENERATION DES RESULTATS
@@ -62,6 +67,17 @@ ylabel('CDF');
 title('CDF de la perméabilité');
 grid on;
 
+% Calculer la PDF pour la distribution log-normale
+pdf_values = lognpdf(permeability_range, mu_log, sigma_log);
+
+% Tracer la PDF
+figure;
+plot(permeability_range, pdf_values, 'LineWidth', 2);
+xlabel('Perméabilité (um^2)');
+ylabel('PDF');
+title('PDF de la perméabilité');
+grid on;
+
 % Calcul de l'intervalle pour u
 u_low = exp(mu_log) - exp(mu_log)/exp(sigma_log);
 u_high = exp(mu_log)*exp(sigma_log) - exp(mu_log);
@@ -71,16 +87,17 @@ fprintf('\nu_low: %f\n', u_low);
 fprintf('\nu_high %f\n', u_high);
 
 % Calculer la plage de valeurs pour la porosité
-x_range = linspace(min(porosity_samples), max(porosity_samples), 1000);
+porosity_range = linspace(min(porosity_samples), max(porosity_samples), 1000);
 
 % Calculer la fonction de densité de probabilité (PDF)
-pdf_values = normpdf(x_range, mean_porosity, std_porosity);
+pdf_values = normpdf(porosity_range, mean_porosity, std_porosity);
 
 % Afficher la courbe de la PDF
 figure;
-plot(x_range, pdf_values, 'LineWidth', 2);
+plot(porosity_range, pdf_values, 'LineWidth', 2);
 xlabel('Porosité');
-ylabel('Densité de probabilité');
-title('Courbe de distribution de la porosité (Échantillonnage LHS)');
+ylabel('PDF');
+title('PDF de la porosité');
+grid on
 
 
